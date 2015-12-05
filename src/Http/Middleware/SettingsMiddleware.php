76a4cee71c37cddf5304f80fb48fd423cc34c1ba
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-05 19:12:26 +0000

    Adding the Settings Middleware

---
+++ src/Http/Middleware/SettingsMiddleware.php
@@ -0,0 +1,57 @@
+<?php namespace Arcanedev\Settings\Http\Middleware;
+
+use Arcanedev\Settings\Contracts\Store as Settings;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class     SettingsMiddleware
+ *
+ * @package  Arcanedev\Settings\Http\Middleware
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class SettingsMiddleware
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create a new save settings middleware
+     *
+     * @param  \Arcanedev\Settings\Contracts\Store  $settings
+     */
+    public function __construct(Settings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure                  $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    /**
+     * Perform any final actions for the request lifecycle.
+     *
+     * @param  \Illuminate\Http\Request                    $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     */
+    public function terminate(Request $request, Response $response)
+    {
+        $this->settings->save();
+    }
+}
