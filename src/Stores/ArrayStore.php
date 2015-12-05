91c00836932e04de3b7b3d0a668b2190bef3d7cf
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-05 19:11:03 +0000

    Adding ArrayStore

---
+++ src/Stores/ArrayStore.php
@@ -0,0 +1,12 @@
+<?php namespace Arcanedev\Settings\Stores;
+
+/**
+ * Class     ArrayStore
+ *
+ * @package  Arcanedev\Settings\Stores
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class ArrayStore extends MemoryStore
+{
+    //
+}
