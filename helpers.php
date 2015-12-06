238095c2c8040d39bb3b5afdb24adf7206b0a84f
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-06 18:49:43 +0000

    Adding settings helper

--- helpers.php
+++ helpers.php
@@ -1,3 +1,13 @@
 <?php
 
-//
+if ( ! function_exists('settings')) {
+    /**
+     * Get the SettingManager instance.
+     *
+     * @return \Arcanedev\Settings\SettingsManager
+     */
+    function settings()
+    {
+        return app('arcanedev.settings.manager');
+    }
+}
